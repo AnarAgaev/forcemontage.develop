cff9806aad80bdab97bd8c91e1c906b15b6b741a
Anar Agaev <anar.n.agaev@gmail.com>
2022-01-28 11:42:54 +0300

    Deployed the basic structure of the project

---
+++ src/templates/default/js/main.js
@@ -0,0 +1,231 @@
+// // Блокируем зум экрана на IOS
+// document.addEventListener(
+//     'touchmove',
+//     function(event) {
+//         event = event.originalEvent || event;
+//
+//         if (event.scale !== 1) {
+//             event.preventDefault();
+//         }
+//     },
+//     false
+// );
+//
+// window.IS_DEBUGGING = true;
+//
+// /* В суперглобальной переменной STORE храним
+//  * все дынные, введенные пользователем
+//  * С помощью проксирования, слушаем
+//  * изменение структуры данных
+//  */
+// window.STORE = {
+//     stepsMap: [
+//         // При инициализации, сохраняем в STORE первый шаг
+//         '#forWhom',
+//     ]
+// };
+//
+// STORE = new Proxy(STORE, {
+//     set: function (target, prop, val) {
+//
+//         const removeAnswerFromStore = ([question, answer]) => {
+//             let prop = getPropFromController(question, answer),
+//                 arrAnswers = target[prop][1],
+//                 idxOfAnswer = arrAnswers.indexOf(answer);
+//
+//             arrAnswers.splice(idxOfAnswer, 1);
+//         };
+//
+//         const addAnswerToStore = ([question, answer]) => {
+//             let prop = getPropFromController(question, answer);
+//
+//             target[prop]
+//                 ? target[prop][1].push(answer)
+//                 : target[prop] = [question, [answer]];
+//         };
+//
+//         if (isPluralController(val)) {
+//             /* Для контроллеров типа checkbox при снятии
+//              * галочки, нужно удалить свойство из STORE.
+//              * Поверяем ,если в STORE уже есть целевой
+//              * ответ, удаляем его, иначе добавляем
+//              */
+//             isAnswerInStore(val)
+//                 ? removeAnswerFromStore(val)
+//                 : addAnswerToStore(val);
+//         } else {
+//             target[prop] = val;
+//         }
+//
+//         /*
+//          * Если в СТОРЕ меняется телефон, то сразу меняем
+//          * телефон во всех полях ввода телефона.
+//          */
+//         if (prop === 'phone') {
+//             updatePhones(val);
+//         }
+//
+//         // /*
+//         //  * Если в СТОРЕ меняется email, то сразу меняем
+//         //  * email во всех полях ввода email адреса.
+//         //  */
+//         // if (prop === 'email') {
+//         //     updateEmails(val);
+//         // }
+//
+//         if (IS_DEBUGGING) {
+//             setTimeout(() => console.log(target), 100);
+//         }
+//
+//         return true;
+//     },
+//
+//     deleteProperty: function(target, prop) {
+//         delete target[prop];
+//
+//         if (IS_DEBUGGING) {
+//             setTimeout(() => console.log(target), 100);
+//         }
+//
+//         return true;
+//     }
+// });
+//
+// const isPluralController = ([question, answer]) => {
+//     return $(`[data-question="${question}"][data-answer="${answer}"]`)
+//         .attr('type') === 'checkbox';
+// };
+//
+// const isAnswerInStore = ([question, answer]) => {
+//     let prop = getPropFromController(question, answer);
+//
+//     if (STORE[prop]) {
+//         return STORE[prop][1].includes(answer);
+//     }
+// };
+//
+// const getPropFromController = (question, answer) => {
+//     return $(`[data-question="${question}"][data-answer="${answer}"]`)
+//         .attr('name');
+// };
+//
+// $(document).ready(() => {
+//
+//     /* Слушаем изменение каждого input
+//      * В случае всплытия события,
+//      * пушим данные в STORE
+//      */
+//     $('input.controller').on('input', e => {
+//         let _this = e.target,
+//             prop = $(_this).attr('name'),
+//             question = $(_this).data('question'),
+//             answer = $(_this).data('answer');
+//
+//         STORE[prop] = [question, answer];
+//
+//         resetAllCheckboxControllers(_this);
+//     });
+//
+//     const resetAllCheckboxControllers = (el) => {
+//         if ($(el).attr('type') === 'radio') {
+//             $('input:checkbox').prop('checked', false);
+//         }
+//     };
+//
+//     // Скролл к первому вопросу
+//     window.scrollToQuestionsStart = () => {
+//         let top = $('#progress').offset().top;
+//
+//         $('body,html').animate(
+//             { scrollTop: top },
+//             1000
+//         );
+//     };
+//
+//     $('.go-to-quiz').on('click', scrollToQuestionsStart);
+//
+//     const lazyLoadVideo = (idContainer) => {
+//         let video = $(idContainer).find('video'),
+//             source = video.find('source'),
+//             src = video.data('src');
+//
+//         video.attr('src', src);
+//         source.attr('src', src);
+//     }
+//     // Ленивая загрузка видео
+//     setTimeout(
+//         () => {
+//             lazyLoadVideo('#presentationModal');
+//             lazyLoadVideo('#getContactVideo');
+//         },
+//         3000
+//     );
+//
+//     // Блокируем отправку всех форм.
+//     // Данные всегда отправляются асинхронно.
+//     $('form').submit(function (e) {
+//         e.preventDefault();
+//
+//         const form = e.target,
+//             submit = $(this).find('[type="submit"]'),
+//             formType = $(this).find('[name="form"]').val();
+//
+//         if (!submit.attr('disabled'))  {
+//
+//             const request = $.ajax({
+//                 method: 'post',
+//                 url: 'https://quiz24.ru/portfolio/raiton/forms-handler.php',
+//                 data: $(form).serialize(),
+//                 dataType: 'json'
+//             });
+//
+//             request.done(response => {
+//                 if (IS_DEBUGGING) console.log(response);
+//
+//                 if (response.error) {
+//                     if (formType === 'consultation') {
+//
+//                         const dialogs = $(form)
+//                             .closest('.modal')
+//                             .find('.modal__dialog');
+//
+//                         $(dialogs[0]).addClass('modal__dialog_hide');
+//
+//                         setTimeout(() => {
+//                             $(dialogs[0]).addClass('hidden');
+//                             $(dialogs[1]).removeClass('hidden');
+//                         }, 300);
+//
+//                         setTimeout(() => {
+//                             $(dialogs[1]).removeClass('modal__dialog_hide');
+//                         }, 400);
+//
+//                     } else if (formType === 'results-consultation') {
+//                         showModal($(form).find('[type="submit"]')[0]);
+//                     }
+//
+//                 } else {
+//                     console.log('Ошибка отправки сообщения в обработчике формы!')
+//                 }
+//             });
+//
+//             request.fail(function( jqXHR, textStatus ) {
+//                 console.log("Request failed: " + jqXHR + " --- " + textStatus);
+//             });
+//         }
+//     });
+// });
+//
+// // Get word form for days
+// window.getCountWordForm = (count) => {
+//     const cases = [2, 0, 1, 1, 1, 2];
+//     const forms = ['матрас', 'матраса', 'матрасов'];
+//
+//     return forms[(count % 100 > 4 && count % 100 < 20)
+//         ? 2
+//         : cases[(count % 10 < 5)
+//             ? count % 10
+//             : 5
+//             ]
+//         ];
+// };
---
+++ src/templates/default/js/masks.js
@@ -0,0 +1,141 @@
+// window.validatePhone = (phone = STORE.phone) => {
+//     let regular = /^(\+7)?(\d{3}?)?[\d]{11}$/;
+//     return regular.test(phone);
+// };
+//
+// window.validateEmail = (email = STORE.email) => {
+//     // Регулярка для email проверяет только
+//     // наличие @ и точки
+//     let regular = /.+@.+\..+/i;
+//     return regular.test(email);
+// };
+//
+// $(document).ready(() => {
+//
+// // ******************** Обработка телефонных номеров -- START
+//
+//     // Маски для всех телефонов сохраняем
+//     // в отдельный суперглобальный массив
+//     const PHONE_MASKS = [];
+//
+//     // Маска для телефона одинакова
+//     // для всех телефонов
+//     const phoneMaskOptions = {
+//         mask: '+{7} ({9}00) 000-00-00',
+//         lazy: true,
+//         placeholderChar: '_'
+//     };
+//
+//     // Котроллеры для телефонов - взаимозависимые поля
+//     window.updatePhones = (phone) => {
+//         $(PHONE_MASKS).each(
+//             idx => PHONE_MASKS[idx].unmaskedValue = phone
+//         );
+//     };
+//
+//     // Если телефон валидный,
+//     // разлокируем кнопки отправки формы
+//     const checkPhones = () => {
+//         const wraps = $('[type="tel"]').closest('form'),
+//             submits = wraps.find('[type="submit"]'),
+//             buttons = wraps.find('[type="button"]'),
+//             isCheckedElems = wraps.find('.checked').length > 0;
+//
+//         submits.prop({disabled: !validatePhone(STORE.phone)});
+//
+//         if (isCheckedElems && validatePhone(STORE.phone)) {
+//             buttons.prop({disabled: false});
+//         } else {
+//             buttons.prop({disabled: true});
+//         }
+//     };
+//
+//     /* Вешаем маску на контроллер каждого телефона
+//      * и сохраняем все маски в массив PHONE_MASKS
+//      *
+//      * Также на каждом контроллере телефона
+//      * обрабатывается события фокус, потеря фокуса
+//      * и изменение значения
+//      *
+//      */
+//     const initialPhoneMasks = (idx, el) => {
+//         PHONE_MASKS.push(IMask(el, phoneMaskOptions));
+//         el.dataset.maskIdx = idx.toString();
+//     };
+//
+//     const handlerPhoneFocus = e => {
+//         const _this = e.target,
+//             idx = $(_this).data('maskIdx');
+//
+//         PHONE_MASKS[idx].updateOptions({
+//             lazy: false,
+//         });
+//     };
+//
+//     const handlerPhoneBlur = e => {
+//         const _this = e.target,
+//             idx = $(_this).data('maskIdx');
+//
+//         PHONE_MASKS[idx].updateOptions({
+//             lazy: true,
+//         });
+//
+//         if (!validatePhone()) {
+//             STORE.phone = ''; // Нужно для организации взаимозависимых полей
+//             delete STORE.phone;
+//         }
+//     };
+//
+//     const handlerPhoneInput = e => {
+//         const _this = e.target,
+//             idx = $(_this).data('maskIdx');
+//
+//         STORE['phone'] = PHONE_MASKS[idx].unmaskedValue;
+//         checkPhones();
+//     };
+//
+//     $('[type="tel"]')
+//         .each(initialPhoneMasks)
+//         .on('focus', handlerPhoneFocus)
+//         .on('blur', handlerPhoneBlur)
+//         .on('input', handlerPhoneInput);
+//
+// // ******************** Обработка телефонных номеров -- FINISH
+//
+// // ******************** Обработка емэйлов -- START
+//
+//     // Котроллеры для телефонов - взаимозависимые поля
+//     // window.updateEmails = (email) => {
+//     //     $('[type="email"]').val(email);
+//     // };
+//
+//     // // Если email валидный, разблокируем
+//     // // кнопки отправки формы
+//     // const checkEmail = () => {
+//     //     $('[type="email"]')
+//     //         .closest('form')
+//     //         .find('[type="submit"]')
+//     //         .prop({
+//     //             disabled: !validateEmail(STORE.email)
+//     //         });
+//     // };
+//
+//
+//     //     const handlerEmailInput = e => {
+//     //         STORE['email'] = $(e.target).val();
+//     //         checkEmail();
+//     //     };
+//
+//     //     const handlerEmailBlur = e => {
+//     //         if (!validateEmail()) {
+//     //             STORE.email = ''; // Нужно для организации взаимозависимых полей
+//     //             delete STORE.email;
+//     //         }
+//     //     };
+//
+//     //     $('[type="email"]')
+//     //         .on('blur', handlerEmailBlur)
+//     //         .on('input', handlerEmailInput);
+//
+// // ******************** Обработка емэйлов -- FINISH
+// });
