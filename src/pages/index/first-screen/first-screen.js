959da85cb5cc5dda4d42510a5c0af2ddd01c2b86
Anar Agaev <anar.n.agaev@gmail.com>
2022-01-31 23:53:14 +0300

    Added first screen

--- src/pages/index/first-screen/first-screen.js
+++ src/pages/index/first-screen/first-screen.js
@@ -1,3 +1,16 @@
 $(document).ready(() => {
+    setTimeout(
+        () => $('#firstScreen').addClass('loaded'),
+        500
+    );
+
+    if ($(window).width() > 767) {
+        const video = $('#firstScreen video'),
+            source = video.find('source'),
+            src = video[0].dataset.startSrc;
+
+        video[0].src = src;
+        source[0].src = src;
+    }
 
 });
